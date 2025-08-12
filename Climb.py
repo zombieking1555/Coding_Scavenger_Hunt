5bc0d0dc478bee548703a861b1a50a60f1c83072
zombieking1555 <156970151+zombieking1555@users.noreply.github.com>
2025-08-11 23:55:46 -0400

    smoothed out wording in climb.py

--- Climb.py
+++ Climb.py
@@ -3,7 +3,7 @@
 You are now at checkpoint 4! Hopefully you remember the first word you got, because you will be finding the second
 one in this file! You will see that each of the functions (Denoted by the word "def", remember!) are being described
 very specifically in this subsystem with print() statements. To find your second of four words, look at the first letter
-of each print statement! Then, look to the print statements to get the next file to navigate to.
+of each print statement! Then, look again to the print statements to get the next file to navigate to.
 
 """
 
